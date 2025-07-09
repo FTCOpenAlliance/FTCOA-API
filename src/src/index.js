4b31ac1dd867fd3929df662d92192d98c9bd01a3
hamidh-2k8 <hamidh.2k8@gmail.com>
2025-07-09 14:29:59 -0400

    First test of HTTP requests and D1 queries (success!)

--- src/src/index.js
+++ src/src/index.js
@@ -1,18 +1,43 @@
 
 var src_default = {
   async fetch(request, env) {
-    const { ftcoatestdb } = env;
+    const db = env.ftcoatestdb;
 
-    const res = await ftcoatestdb.prepare(
-      "SELECT * FROM Teams"
-    ).run()
+    const queryString = new URL(request.url).pathname
 
-    console.log((res.results))
+    let isQueryStringValid = false
 
-    return new Response(JSON.stringify(res.results), {
+    let responseString = "Error 404: The resource specified cannot be found."
+
+    if (queryString == "/teamsSimple") {
+
+      isQueryStringValid = true
+
+      const res = await db.prepare(
+        "SELECT * FROM Teams"
+      ).run()
+
+      responseString = JSON.stringify(res.results)
+
+    }
+
+    if (queryString == "/teamLinks") {
+
+      isQueryStringValid = true
+
+      const res = await db.prepare(
+        "SELECT * FROM TEAMLINKS"
+      ).run()
+
+      responseString = JSON.stringify(res.results)
+
+    }
+
+    return new Response(responseString, {
       headers: {
-        "content-type": "application/json; charset=UTF-8"
-      }
+        "content-type": isQueryStringValid ? "application/json" : "text/html"
+      },
+      status: isQueryStringValid ? 200 : 404
     });
   }
 };
