616deb9c3106243c090504d01ceee8bc0a27797b
hamidh-2k8 <hamidh.2k8@gmail.com>
2025-07-10 14:13:48 -0400

    added an actual router

--- src/src/index.js
+++ src/src/index.js
@@ -1,53 +1,45 @@
-var src_default = {
-  async fetch(request, env) {
-    const db = env.ftcoatestdb;
+import { Router } from '@tsndr/cloudflare-worker-router'
 
-    const queryString = new URL(request.url).pathname
+const router = new Router()
 
-    let isQueryStringValid = false
+let JSONHeader = new Headers({"Content-Type": "application/json"})
 
-    // DEFAULT RESPONSE
-    let responseString = "Error 404: The resource specified cannot be found."
+let db
 
+router.debug()
 
-    // RESPONSE FOR /teamsSimple
-    if (queryString == "/teamsSimple") {
+router.cors()
 
-      isQueryStringValid = true
+router.get('/hello', async () => {
+    return new Response(`
+      <h1>Hello, World!</h1>
+      <p>You've successfully accessed the FTC Open Alliance API.</p>`,
+      {headers: new Headers({"Content-Type": "text/html"})})
+})
 
-      const res = await db.prepare(
-        "SELECT * FROM Teams"
-      ).run()
+router.get('/teams/listSimple', async () => {
 
-      responseString = JSON.stringify(res.results)
+    let data = await db.prepare("SELECT * FROM Teams").run()
 
+    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
+})
+
+router.get('/teams/:teamnumber/simple', async ({ req }) => {
+
+    let data = await db.prepare("SELECT * FROM Teams WHERE TeamNumber IS " + req.params.teamnumber).run()
+
+    if (data.results == '') {return new Response('Team does not exist.', {status: 400})}
+
+    console.log(data)
+
+    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
+
+})
+
+export default {
+    async fetch(request, env, ctx) {
+
+        db = env.ftcoatestdb
+        return router.handle(request, env, ctx)
     }
-
-
-    // RESPONSE FOR /teamLinks
-    if (queryString == "/teamLinks") {
-
-      isQueryStringValid = true
-
-      const res = await db.prepare(
-        "SELECT * FROM TEAMLINKS"
-      ).run()
-
-      responseString = JSON.stringify(res.results)
-
-    }
-
-
-
-    return new Response(responseString, {
-      headers: {
-        "content-type": isQueryStringValid ? "application/json" : "text/html"
-      },
-      status: isQueryStringValid ? 200 : 404
-    });
-  }
-};
-
-export {
-  src_default as default
-};
+}
