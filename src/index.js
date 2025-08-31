14c68ab96597be3c4c2a755ad923c42ae2fcc75f
hamidh-2k8 <hamidh.2k8@gmail.com>
2025-08-31 19:58:05 -0400

    Added full read/writes for form and profile pages, changed basic requests to include link data by default.

--- src/index.js
+++ src/index.js
@@ -11,44 +11,46 @@ app.get('/', async () => {
       <h1>Hello, World!</h1>
       <p>You've successfully accessed the FTC Open Alliance API.</p>`,
         {headers: new Headers({"Content-Type": "text/html"})})
-})
+    })
     
 app.get('/teams', async () => {
-    
-    let data = await db.prepare("SELECT * FROM Teams").run()
-    
-    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
-})
-
-app.get('/teams/:teamnumber', async (c) => {
-    
-    let data = await db.prepare("SELECT * FROM Teams WHERE TeamNumber IS ?")
-    .bind(c.req.param('teamnumber'))
-    .run()
-    
-    if (data.results == '') {return new Response('Team does not exist.', {status: 400})}
-    
-    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
-    
-})
-
-app.get('/teams/:teamnumber/links', async (c) => {
-    
-    let data = await db.prepare("SELECT * FROM TeamLinks WHERE TeamNumber IS ?")
-    .bind(c.req.param('teamnumber'))
-    .run()
-    
-    if (data.results == '') {return new Response('Team does not exist.', {status: 400})}
-    
-    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
-    
-})
-
-app.get('/internal/teamListData', async () => {
     
     let data = await db.prepare("SELECT * FROM Teams LEFT JOIN TeamLinks ON Teams.TeamNumber = TeamLinks.TeamNumber").run()
     
     return new Response(JSON.stringify(data.results), {headers: JSONHeader})
+})
+    
+app.get('/teams/:teamnumber', async (c) => {
+    
+    let data = await db.prepare("SELECT * FROM Teams WHERE TeamNumber IS ? LEFT JOIN TeamLinks ON Teams.TeamNumber = TeamLinks.TeamNumber")
+    .bind(c.req.param('teamnumber'))
+    .run()
+    
+    if (data.results == '') {return new Response('Team does not exist.', {status: 400})}
+    
+    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
+    
+})
+
+app.get('/teams/:teamnumber/all', async (c) => {
+
+    if (isNaN(formData.teamNumber)) {return new Response('Team Number Invalid.', {status: 400})}
+    
+    let data = await db.prepare(`
+        SELECT * FROM Teams WHERE TeamNumber IS ?
+        LEFT JOIN TeamLinks ON Teams.TeamNumber = TeamLinks.TeamNumber
+        LEFT JOIN TeamInfo ON Teams.TeamNumber = TeamInfo.TeamNumber
+        LEFT JOIN RobotInfo ON Teams.TeamNumber = RobotInfo.TeamNumber
+        LEFT JOIN CodeInfo ON Teams.TeamNumber = CodeInfo.TeamNumber
+        LEFT JOIN FreeResponse ON Teams.TeamNumber = FreeResponse.TeamNumber
+        `)
+    .bind(c.req.param('teamnumber'))
+    .run()
+    
+    if (data.results == '') {return new Response('Team does not exist.', {status: 400})}
+    
+    return new Response(JSON.stringify(data.results), {headers: JSONHeader})
+    
 })
 
 app.post('/internal/formSubmission', async (c) => {
@@ -56,8 +58,9 @@ app.post('/internal/formSubmission', async (c) => {
     let formData = await c.req.json()
     
     if (isNaN(formData.teamNumber)) {return new Response('Team Number Invalid.', {status: 400})}
+
     try {
-        //Team Info
+        //Team Identification
         await db.prepare("INSERT OR REPLACE INTO Teams (TeamName, TeamNumber) VALUES (?, ?)")
         .bind(formData.teamName, formData.teamNumber)
         .run()
@@ -66,7 +69,27 @@ app.post('/internal/formSubmission', async (c) => {
         await db.prepare("INSERT OR REPLACE INTO TeamLinks (TeamNumber, BuildThread, CAD, Code, Photo, Video, TeamWebsite) VALUES (?, ?, ?, ?, ?, ?, ?)")
         .bind(formData.teamNumber, (formData.buildThread || null), (formData.cadLink || null), (formData.codeLink || null), (formData.photoLink || null), (formData.videoLink || null), (formData.teamWebsite || null))
         .run()
-        
+
+        //Team Info
+        await db.prepare("INSERT OR REPLACE INTO TeamInfo (TeamNumber, RookieYear, TeamMembers, Mentors, TeamType, MeetingHours, Budget, Workspace, Sponsors) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)")
+        .bind(formData.teamNumber, (formData.rookieYear || null), (formData.teamMembers || null), (formData.mentors || null), (formData.teamType || null), (formData.meetingHours || null), (formData.budget || null), (formData.workspace || null), (formData.sponsors || null))
+        .run()
+
+        //Robot Info
+        await db.prepare("INSERT OR REPLACE INTO RobotInfo (TeamNumber, Drivetrain, Materials, Products, Systems, Odometry, Sensors) VALUES (?, ?, ?, ?, ?, ?, ?)")
+        .bind(formData.teamNumber, (formData.drivetrain || null), (formData.materials || null), (formData.products || null), (formData.systems || null), (formData.odometry || null), (formData.sensors || null))
+        .run()
+
+        //Code Info
+        await db.prepare("INSERT OR REPLACE INTO CodeInfo (TeamNumber, CodeLang, CodeEnv, CodeTools, Vision) VALUES (?, ?, ?, ?, ?)")
+        .bind(formData.teamNumber, (formData.codeLang || null), (formData.codeEnv || null), (formData.codeTools || null), (formData.vision || null))
+        .run()
+
+        //Free Response
+        await db.prepare("INSERT OR REPLACE INTO FreeResponse (TeamNumber, UniqueFeatures, Outreach, CodeAdvantage, Competitions, TeamStrategy, GameStrategy, DesignProcess) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
+        .bind(formData.teamNumber, (formData.uniqueFeatures || null), (formData.outreach || null), (formData.codeAdvantage || null), (formData.competitions || null), (formData.teamStrategy || null), (formData.gameStrategy || null), (formData.designProcess || null))
+        .run()
+
         return new Response(`Updated Data for team ${formData.teamNumber}`, {status: 200})
         
     } catch (e) {
